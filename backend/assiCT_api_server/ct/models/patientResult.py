631320c1e5ebab44d3c2cd973d0dbf18ebec91b5
Gitko97 <xcvdsf8216@gmail.com>
2022-06-30 20:50:11 +0900

    오타 수정 d->t

--- backend/assiCT_api_server/ct/models/patientResult.py
+++ backend/assiCT_api_server/ct/models/patientResult.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class PatientResult(models.Model):
     createdDate = models.DateTimeField(auto_now_add=True)
-    patiendName = models.CharField(max_length=10)
+    patientName = models.CharField(max_length=10)
     note = models.CharField(max_length=256)
