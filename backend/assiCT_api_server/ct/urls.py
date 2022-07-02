32e93724e32045386de573253bbbc6e98621bfd0
Gitko97 <xcvdsf8216@gmail.com>
2022-07-03 01:15:15 +0900

    api 폴더 생성

--- backend/assiCT_api_server/ct/urls.py
+++ backend/assiCT_api_server/ct/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .api import file_upload
 
 urlpatterns = [
-    path('fileUpload', views.dicom_file_upload),
+    path('fileUpload', file_upload.dicom_file_upload),
 ]
