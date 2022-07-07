32af2b2c8c89ff6d4dc894dd56ac13b13206ccaf
Minji Kim <minji1289@naver.com>
2022-07-08 02:07:43 +0900

    Hospital pk modified & migrated

--- backend/assiCT_api_server/user/models/hospital.py
+++ backend/assiCT_api_server/user/models/hospital.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Hospital(models.Model):
-    hospitalCode = models.CharField(max_length=50, unique=True)
+    hospitalCode = models.CharField(primary_key=True, max_length=50, unique=True)
     hospitalName = models.CharField(max_length=200)
     hospitalAddress = models.CharField(max_length=300)
