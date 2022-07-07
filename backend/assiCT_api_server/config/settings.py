4d354145661a6ef5d32a5fd213eb0b11c5de7712
Gitko97 <xcvdsf8216@gmail.com>
2022-07-07 15:44:23 +0900

    DB_PORT -> .env 파일

--- backend/assiCT_api_server/config/settings.py
+++ backend/assiCT_api_server/config/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
         'HOST': env('DB_HOST'),
-        'PORT': '5432',
+        'PORT': env('DB_PORT'),
     }
 }
 
