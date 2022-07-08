444952ae4427593a081a29254bf7c2342360f17a
grdnr13 <grdnr17@gmail.com>
2022-07-08 17:11:02 +0900

    Configuration - 공유버튼 제거
    불필요한 버튼 제거

--- frontend/assict_front/src/components/Configurator/Configurator.js
+++ frontend/assict_front/src/components/Configurator/Configurator.js
@@ -64,7 +64,7 @@ export default function Configurator(props) {
           <DrawerHeader pt='24px' px='24px'>
             <DrawerCloseButton color='white' />
             <Text color='white' fontSize='xl' fontWeight='bold' mt='16px'>
-              Vision UI Configurator
+              AssiCT
             </Text>
             <Text color='white' fontSize='md' mb='16px'>
               See your dashboard options.
@@ -97,74 +97,12 @@ export default function Configurator(props) {
                 </DarkMode>
               </Box>
               <Box>
-                <Box>
-                  <Link
-                    href='https://www.creative-tim.com/product/vision-ui-dashboard-chakra'
-                    w='100%'
-                    mb='16px'>
-                    <Button
-                      w='100%'
-                      mb='16px'
-                      bg='brand.200'
-                      color={colorButton}
-                      fontSize='xs'
-                      variant='brand'
-                      px='30px'>
-                      Free Download
-                    </Button>
-                  </Link>
-                  <Link
-                    href='https://demos.creative-tim.com/docs-vision-ui-dashboard-chakra/'
-                    w='100%'>
-                    <Button
-                      w='100%'
-                      color={secondaryButtonColor}
-                      fontSize='xs'
-                      variant='outlineWhite'
-                      px='20px'
-                      mb='16px'>
-                      <Text textDecorationColor='none'>Documentation</Text>
-                    </Button>
-                  </Link>
-                </Box>
-                <Flex
-                  justifyContent='center'
-                  alignItems='center'
-                  w='100%'
-                  mb='16px'>
-                  <GitHubButton
-                    href='https://github.com/creativetimofficial/vision-ui-dashboard-chakra'
-                    data-icon='octicon-star'
-                    data-show-count='true'
-                    aria-label='Star creativetimofficial/vision-ui-dashboard-chakra on GitHub'>
-                    Star
-                  </GitHubButton>
-                </Flex>
                 <Box w='100%'>
                   <Text color='white' mb='6px' textAlign='center'>
                     Thank you for sharing!
                   </Text>
                   <LightMode>
                     <Flex justifyContent='center' alignContent='center'>
-                      <Link
-                        isExternal='true'
-                        href='https://twitter.com/intent/tweet?url=https%3A%2F%2Fwww.creative-tim.com%2Fproduct%2Fvision-ui-dashboard-chakra&text=Check%20vision%20UI%20Dashboard%20made%20by%20%40CreativeTim%20and%20%40simmmple_web%20%23webdesign%20%23dashboard%20%23chakra'>
-                        <Button
-                          colorScheme='twitter'
-                          leftIcon={<FaTwitter />}
-                          me='10px'>
-                          <Text>Tweet</Text>
-                        </Button>
-                      </Link>
-                      <Link
-                        isExternal='true'
-                        href='https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.creative-tim.com%2Fproduct%2Fvision-ui-dashboard-chakra'>
-                        <Button
-                          colorScheme='facebook'
-                          leftIcon={<FaFacebook />}>
-                          <Text>Share</Text>
-                        </Button>
-                      </Link>
                     </Flex>
                   </LightMode>
                 </Box>
