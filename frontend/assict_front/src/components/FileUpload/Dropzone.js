5ab8407e489f0a8c47701b97f63702cf752014d1
grdnr13 <grdnr17@gmail.com>
2022-07-11 07:18:36 +0900

    Create Dropzone.js

---
+++ frontend/assict_front/src/components/FileUpload/Dropzone.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {useDropzone} from 'react-dropzone';
+
+function Dropzone(props) {
+  const {getRootProps, getInputProps, open, acceptedFiles} = useDropzone({
+    // Disable click and keydown behavior
+    noClick: true,
+    noKeyboard: true
+  });
+
+  const files = acceptedFiles.map(file => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ));
+
+  return (
+    <div className="container">
+      <div {...getRootProps({className: 'dropzone'})}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here</p>
+        <button type="button" onClick={open}>
+          Open File Dialog
+        </button>
+      </div>
+      <aside>
+        <h4>Files</h4>
+        <ul>{files}</ul>
+      </aside>
+    </div>
+  );
+}
+
+export default Dropzone;
