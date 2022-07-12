0f61a0380114cdb35ddf65de8a557f79e8ba21ae
Gitko97 <xcvdsf8216@gmail.com>
2022-07-12 16:31:05 +0900

    Dropzone file method

--- frontend/assict_front/src/components/FileUpload/Dropzone.js
+++ frontend/assict_front/src/components/FileUpload/Dropzone.js
@@ -2,31 +2,23 @@ import React, {UseState} from 'react';
 import {useDropzone} from 'react-dropzone';
 import {Text, Button} from "@chakra-ui/react";
 import Card from "../Card/Card";
-import CardBody from "../Card/CardBody";
-import MainPanel from "../Layout/MainPanel";
 import {Separator} from "../Separator/Separator";
 import axios from 'axios';
 
+
+function onFileUpload(event, current_files){
+  const formData = new FormData();
+  event.preventDefault();
+  console.log(current_files);
+
+  //axios await patientResult -> id return (성공했을 때)
+  // 위 id 값으로 current_files 하나씩 http://{{django_server}}:8000/api/ct/fileUpload/1
+}
 
 
 function Dropzone(props) {
   // const [inputFiles, setInputFiles] = useState(null);	//파일
 
-  const axios = require('axios');
-
-  let config = {
-    method: 'post',
-    url: 'http://127.0.0.1:8000/api/ct/patientResult',
-    headers: { }
-  };
-
-  axios(config)
-  .then(function (response) {
-    console.log(JSON.stringify(response.data));
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
 
   const {getRootProps, getInputProps, open, acceptedFiles} = useDropzone({
     // Disable click and keydown behavior
@@ -41,22 +33,6 @@ function Dropzone(props) {
 
   ));
 
-
-  function onFileUpload(event){
-    const formData = new FormData();
-    event.preventDefault();
-    console.log(acceptedFiles);
-    // formData.append(
-    //   "demo file",
-    //   this.state.selectedFile,
-    //   this.state.selectedFile.name
-    // )
-    // axios.post("", formData).then(() => { //URL - 나중에 다른 파일에 저장하고 import해서 이용하는거로 바꾸기
-    //   this.setState({selectedFile: null});
-    //   this.setState({fileUploadedSuccessfully: true});
-    // })
-  }
-
   return (
 
     <div className="container">
@@ -67,7 +43,7 @@ function Dropzone(props) {
           Drag and drop files here
 
         </Card>
-        <Button variant='outline'>
+        <Button type="file" multiple variant='outline'>
             Open File Dialog
         </Button>
 
@@ -78,29 +54,10 @@ function Dropzone(props) {
         <ul>{files}</ul>
 
       <div className="App">
-        <form>
-          <div>
-            <input type="file" multiple onChange={async (event) => {
-              event.preventDefault();
-              let fileList = [];
-              for (let i = 0; i < event.target.files.length; i++) {
-                fileList.push(event.target.files[i])
-              }
 
-              const fileSend = await axios({
-                method: 'get',
-                url: '',
-                data: {
-                  fileList
-                }
-              })
-            }}/>
-
-          </div>
-          <div>
-            <button className="btn" type="submit" onClick={event => onFileUpload(event)}>Upload</button>
-          </div>
-        </form>
+        <div>
+          <button className="btn" type="submit" onClick={event => onFileUpload(event, acceptedFiles)}>Upload</button>
+        </div>
 
         {/*<input type="file" name="imgFile" id="imgFile" onChange={handleChangeFile}/>*/}
         {/*<input type="submit" value="Upload File" />*/}
