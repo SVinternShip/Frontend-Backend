1ce92aef100800eff328759053a5c5efb5c6ffda
Gitko97 <xcvdsf8216@gmail.com>
2022-07-12 22:16:50 +0900

    fileUpload 페이지 완성 (추후 console.log 제거)

--- frontend/assict_front/src/components/FileUpload/Dropzone.js
+++ frontend/assict_front/src/components/FileUpload/Dropzone.js
@@ -5,59 +5,58 @@ import Card from "../Card/Card";
 import {Separator} from "../Separator/Separator";
 import axios from 'axios';
 
-async function onFileUpload(event, current_files) {
-  const formData = new FormData();
-  event.preventDefault();
-  console.log(current_files);
-  // const patientResult = await axios({
-  //   method: 'get',
-  //   url: 'http://127.0.0.1:8000/api/ct/patientResult',
-  //   headers: { }
-  // })
-
-  let fileList = [];
-  for (let i = 0; i < current_files.length; i++) {
-    fileList.push(current_files[i]);
-  }
-
-  let axios = require('axios');
+function upLoadDcm(file,token, patient_result_id){
+  let data = new FormData();
+  data.append('file', file);
   let config = {
-    method: 'post',
-    url: 'http://127.0.0.1:8000/api/ct/patientResult',
-    headers: {}
+    "method": 'post',
+    "url": 'http://127.0.0.1:8000/api/ct/fileUpload/'+patient_result_id,
+    "headers": {
+      'Authorization': token,
+      'Content-Type': 'multipart/form-data'
+    },
+    "data" : data
   };
-  let patientNum;
   axios(config)
-  .then(function (response) {
-    console.log(JSON.stringify(response.data));
-    patientNum = response["id"];
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+}
 
-  let uploadUrl = 'http://{{django_server}}:8000/api/ct/fileUpload/'+ patientNum;
-
-  const fileSend = await axios({
-    method: 'get',
-    url: uploadUrl,
-    data: {
-      fileList
+async function onFileUpload(event, current_files) {
+  event.preventDefault();
+  if (current_files.length < 1)
+    alert("Selected file zero")
+    return
+  const token = 'JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImFzc2ljdCIsImV4cCI6MTY1ODIyNzU2MCwiZW1haWwiOiIiLCJvcmlnX2lhdCI6MTY1NzYyMjc2MH0.V1EV7pqpg_YCIhBlfxVrhbil5LCyTqZt194K0N0Rv4s'
+  let config = {
+    "method": 'post',
+    "url": 'http://127.0.0.1:8000/api/ct/patientResult',
+    "headers": {
+      'Authorization': token
     }
-  })
+  };
 
-  //axios await patientResult -> id return (성공했을 때)
-  // 위 id 값으로 current_files 하나씩 http://{{django_server}}:8000/api/ct/fileUpload/1
+  try{
+    const response = await axios(config)
+    console.log(response)
+    let patient_result_id = response.data['id']
+    for(let i=0; i<current_files.length;i++){
+      upLoadDcm(current_files[i], token, patient_result_id)
+    }
+  }catch (err) {
+    
+  }
 }
 
 
+
 function Dropzone(props) {
-  // const [inputFiles, setInputFiles] = useState(null);	//파일
-
-
   const {getRootProps, getInputProps, open, acceptedFiles} = useDropzone({
-    // Disable click and keydown behavior
-    noClick: true,
+    noClick: false,
     noKeyboard: true
   });
 
@@ -75,13 +74,8 @@ function Dropzone(props) {
       <div {...getRootProps({className: 'dropzone'})} >
         <input {...getInputProps()} />
         <Card overflowX={{ xl: "hidden" }} my='22px' pb='100px' placeholder="Drag and drop files here">
-          Drag and drop files here
-
+          <a>Double click to open dialog or Drag your files</a>
         </Card>
-        <Button type="file" multiple variant='outline'>
-            Open File Dialog
-        </Button>
-
       </div>
 
         <h4>Files</h4>
@@ -89,14 +83,9 @@ function Dropzone(props) {
         <ul>{files}</ul>
 
       <div className="App">
-
         <div>
           <button className="btn" type="submit" onClick={event => onFileUpload(event, acceptedFiles)}>Upload</button>
         </div>
-
-        {/*<input type="file" name="imgFile" id="imgFile" onChange={handleChangeFile}/>*/}
-        {/*<input type="submit" value="Upload File" />*/}
-
       </div>
 
 
